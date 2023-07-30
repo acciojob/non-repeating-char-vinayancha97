43d46677c758f72330346e9fdd674aed689dc6a7
vinayancha97 <135926111+vinayancha97@users.noreply.github.com>
2023-07-30 10:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let s = prompt("Enter a string");
+function firstNonRepeatingCharacter(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+      return char;
+    }
+  }
+  return null;
+}
+alert(firstNonRepeatingCharacter(s));
